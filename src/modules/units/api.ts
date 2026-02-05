8fe7163d93498d726f70f52ff84bad8210a63619
dangwd <dangwd.vicidev@gmail.com>
2026-02-05 14:30:24 +0700

    update

--- src/modules/units/api.ts
+++ src/modules/units/api.ts
@@ -3,4 +3,10 @@ const BASE_URL = `UnitOfMeasures`
 export const uomApi = {
   get: () =>
     httpClient.get(`${BASE_URL}`),
+  create: () =>
+    httpClient.post(`${BASE_URL}`),
+  update: (id: string) =>
+    httpClient.put(`${BASE_URL}/${id}`),
+  delete: (id: string) =>
+    httpClient.delete(`${BASE_URL}/${id}`)
 };
